39dc4502ae5744c91dbb3bdb837928426efcb498
Keith Brinks <keith@brinks.me>
2025-12-15 21:29:05 -0500

    Add php8.2-mysql and php8.2-redis to install script
    The install.sh script now includes php8.2-mysql and php8.2-redis extensions in the PHP installation step to support MySQL and Redis functionality.

--- install.sh
+++ install.sh
@@ -27,8 +27,9 @@ add-apt-repository -y ppa:ondrej/php
 apt-get update
 
 # Install PHP 8.2 and extensions
-apt-get install -y php8.2 php8.2-cli php8.2-common php8.2-sqlite3 php8.2-curl \
-    php8.2-mbstring php8.2-xml php8.2-zip php8.2-bcmath php8.2-intl php8.2-fpm
+apt-get install -y php8.2 php8.2-cli php8.2-common php8.2-sqlite3 php8.2-mysql \
+    php8.2-curl php8.2-mbstring php8.2-xml php8.2-zip php8.2-bcmath php8.2-intl \
+    php8.2-fpm php8.2-redis
 
 # Install Composer
 if ! command -v composer &> /dev/null; then
